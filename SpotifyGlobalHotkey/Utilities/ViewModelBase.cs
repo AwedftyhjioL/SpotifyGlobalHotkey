04a06f3effbb78c983917eb2527a6e99accfa960
AwedftyhjioL <youkequan@outlook.com>
2024-03-10 19:44:54 +0800

    feat: Page Navigation

---
+++ SpotifyGlobalHotkey/Utilities/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SpotifyGlobalHotkey.Utilities
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
---
+++ SpotifyGlobalHotkey/ViewModel/NavigationVM.cs
@@ -0,0 +1,39 @@
+﻿using SpotifyGlobalHotkey.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SpotifyGlobalHotkey.ViewModel
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; onPropertyChanged(); }
+        }
+
+        public ICommand HomeCommand { get; set; }
+
+        public ICommand SettingsCommand { get; set; }
+
+        private void Home(object obj) => CurrentView = new HomeVM();
+        
+        private void Settings(object obj) => CurrentView = new SettingsVM();
+
+        public NavigationVM()
+        {
+            HomeCommand = new RelayCommand(Home);
+            SettingsCommand = new RelayCommand(Settings);
+
+            //Startup Page
+            CurrentView = new HomeVM();
+        }
+
+    }
+}
